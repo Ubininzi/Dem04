73ade4dd68151fcce88ff6101aa2618ebd549f84
Максим Шабанов <maksim20048.ru@gmail.com>
2024-01-05 07:26:34 +0400

    Добавьте файлы проекта.

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace dem04
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
