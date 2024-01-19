e632b332f492304a63003f3c90546cb2e90439fd
ST307-07 <ST307-07@W19-307-07>
2024-01-19 14:24:40 +0400

    usercontrol design

--- windows/MainWindow.xaml.cs
+++ windows/MainWindow.xaml.cs
@@ -1,12 +1,39 @@
-﻿using System.Windows;
+﻿using dem04.EFModel;
+using dem04.UserControls;
+using Microsoft.EntityFrameworkCore;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace dem04
 {
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			MainGrid = FillMainGrid();
+		}
+		public Grid FillMainGrid() { 
+			Grid grid = new Grid();
+			Dem04DbContext db = new Dem04DbContext();
+			int i = 0, j = 0;
+			foreach (Request request in db.Requests.Include(r => r.Worker).Include(r => r.Client).Include(r => r.Equipment))
+			{
+				RequestUserControl newRequest = new RequestUserControl(request);
+
+				Grid.SetColumn(newRequest, i);
+				Grid.SetRow(newRequest, j);
+				grid.Children.Add(newRequest);
+
+				i++;
+				if (i >= 5)
+				{
+					RowDefinition row = new RowDefinition();
+					grid.RowDefinitions.Add(row);
+					i = 0; j++;
+				}
+			}
+			return grid;
+		}
+	}
 }
